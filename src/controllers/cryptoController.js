c1df407f63b6fd78d0eb41636d70d2099cb7e49a
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-06-25 10:40:48 +0300

    add edit page

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -22,6 +22,39 @@ cryptoController.post(`/create`, async (req, res) => {
     }
 })
 
+cryptoController.get(`/details/:cryptoId`, async (req, res) => {
+    const cryptoId = req.params.cryptoId
+    const currentCrypto = await getOneDetailed(cryptoId, "boughtBy").lean()
+    const currentCryptoUnleaned = await getOneById(cryptoId)
+    const ownerId = currentCrypto.owner
+    const currentUser = req.user?._id
+    const isOwner = ownerId == currentUser
+
+    const isBoughtByUser = currentCryptoUnleaned.boughtBy.includes(currentUser)
+    // const tenants = currentHouse.rentedAHome.map(x=> x.name).join(`, `)
+
+    res.render(`crypto/details`, { ...currentCrypto, isOwner })
+})
+
+cryptoController.get(`/edit/:cryptoId`, async (req, res) => {
+    const cryptoId = req.params.cryptoId
+    const currentCrypto = await getOneById(cryptoId).lean()
+    currentCrypto[`${currentCrypto.paymentMethod}`] = true
+    res.render(`crypto/edit`, currentCrypto)
+})
+
+cryptoController.post(`/edit/:cryptoId`, async (req, res) => {
+    const updatedCryptoData = req.body
+    const currentCryptoId = req.params.cryptoId
+
+    try {
+        await findOneByIdAndUpdate(currentCryptoId, updatedCryptoData)
+        res.redirect(`/crypto/details/${currentCryptoId}`)
+    } catch (error) {
+        res.render(`crypto/edit`, { error })
+    }
+})
+
 
 module.exports = cryptoController
 
